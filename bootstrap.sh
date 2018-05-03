9077b84c60da8a98e6723358459f810cb09504f6
Mirko <mirko.covizzi@student.unife.it>
2018-05-03 10:25:14 +0200

    Added updated nodjs and yarn dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev sqlite3 libsqlite3-dev nodejs -y
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev sqlite3 libsqlite3-dev -y
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get update && sudo apt-get install -y nodejs
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
